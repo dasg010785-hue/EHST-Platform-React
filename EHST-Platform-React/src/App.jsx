4d9d932dd7444520dd74ec1f195b81b0f2f4095f
Diego Sanchez <dasg010785@gmail.com>
2026-07-15 14:48:32 -0500

    Rediseño sección Strategy y correcciones de estructura

--- EHST-Platform-React/src/App.jsx
+++ EHST-Platform-React/src/App.jsx
@@ -1,7 +1,19 @@
 import Home from "./pages/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <div
+      className="
+        min-h-screen
+        bg-gradient-to-b
+        from-[#EEF5FF]
+        via-white
+        to-[#FFF1F2]
+      "
+    >
+      <Home />
+    </div>
+  );
 }
 
 export default App;
